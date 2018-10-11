3a16781b6fe15805db7af9a1dffe9d866005224b
Eloi <streeeloi@gmail.com>
2018-10-11 17:16:41 +0200

    11_10_18 grab done

--- Arms_Grab/Assets/Script/Grab.cs
+++ Arms_Grab/Assets/Script/Grab.cs
@@ -7,54 +7,58 @@ public class Grab : MonoBehaviour {
 
     Vector3 offset;
     bool grab = false;
-    Queue<Vector3> lastPosition;
-    [SerializeField]
-    GameObject leftAnkor;
+    
 
-    private void Start()
-    {
-        //init the queue of last positions
-        lastPosition = new Queue<Vector3>();
-    }
+
+   
 
     private void OnTriggerStay(Collider other)
     {
+        float Axe_indexTrigger = Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger");
+        Debug.Log("test is on the trigger-- "+Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger"));
         //check if we grab the ball
-        if (
-            (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
-            && other.CompareTag("pickupAble"))
-        {
+        if (Axe_indexTrigger > 0.1f)
+            ///*&& other.CompareTag("pickupAble")*/)
+      {
+            Debug.Log("test dans if grab");
             //if not already grabed, we said it's grab
             if (!grab)
             {
-                grab = true;
-                //remove gravity and add the kinematic option
-                other.GetComponent<Rigidbody>().useGravity = false;
-                other.GetComponent<Rigidbody>().isKinematic = true;
-            }
-            //set the position of the ball into the hand
-            other.transform.position = gameObject.transform.position +
-                (other.GetComponent<Transform>().position.x *
-                (-gameObject.transform.right + (-gameObject.transform.up) / 5));
+                Debug.Log("this is", other);
 
-            //remove the position too much
-            if (lastPosition.Count >= 10)
-            {
-                lastPosition.Dequeue();
+                grab = true;
+                GiveGravityToObject(other, false);
             }
-            //add the recent position
-            lastPosition.Enqueue(other.transform.position);
+            else
+            {
+                //set the position of the ball into the hand
+                DefineObjectPosition(other);
+            }
         }
         else
         {
             if (grab)
             {
-                //re-add the gravity and remove the kinematic option
-                other.GetComponent<Rigidbody>().useGravity = true;
-                other.GetComponent<Rigidbody>().isKinematic = false;
+
+                GiveGravityToObject(other, true);
             }
             //set grab to false when we leave the ball
             grab = false;
         }
+    }
+
+    private void DefineObjectPosition(Collider other)
+    {
+        other.transform.position = gameObject.transform.position +
+            (gameObject.GetComponent<Transform>().position.x *
+            (-gameObject.transform.right + (-gameObject.transform.up) / 5));
+    }
+
+    private static void GiveGravityToObject(Collider other, bool gravityOn)
+    {
+        //remove gravity and add the kinematic option
+        Rigidbody r = other.attachedRigidbody;
+        r.useGravity = gravityOn;
+        r.isKinematic = !gravityOn;
     }
 }
---
+++ Arms_Grab/Assets/Script/GrabObject.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GrabObject : MonoBehaviour {
+
+    public bool m_isTrigger;
+    private GameObject m_objet;
+    
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        //RaycastHit l_hit;
+
+        if (m_objet)
+        {
+            if (m_isTrigger)
+            {
+                m_objet.GetComponent<Rigidbody>().useGravity = false;
+                Drag();
+            }
+            else
+            {
+                m_objet.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("pickupAble"))
+        {
+            m_objet = other.gameObject;
+        }
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+        {
+            m_isTrigger = true;
+        }
+        
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    m_isTrigger = false;
+    //}
+
+
+    private void Drag()
+    {
+
+        if (IsDragging())
+        {
+            m_objet.transform.parent = transform;
+            m_objet.GetComponent<Collider>().isTrigger = true;
+            m_objet.GetComponent<Rigidbody>().isKinematic = true;
+        }
+        else if (OVRInput.GetUp(OVRInput.RawButton.RHandTrigger))
+        {
+            m_objet.transform.parent = null;
+            m_objet.GetComponent<Collider>().isTrigger = false;
+            m_objet.GetComponent<Rigidbody>().isKinematic = false;
+            m_isTrigger = false;
+
+        }
+    }
+
+    protected abstract bool IsDragging();
+}
+
+public class GrabObjectDefaultExample : GrabObject
+{
+    public KeyCode m_keyToGrab;
+    protected override bool IsDragging()
+    {
+        return Input.GetKey(m_keyToGrab);
+    }
+}
